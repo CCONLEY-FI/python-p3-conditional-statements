236856ffbd6bde02b0e1fcda77fbd01938466b21
Chris <conley.christopher0@gmail.com>
2024-01-29 15:56:36 -0700

    checkmarks

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@
   password. If the username is "admin" or "ADMIN" and the password is "12345",
   return "Access granted". Otherwise, return "Access denied".
 */
-function adminLogin(username, password) {
-  if ((username === "admin" || username === "ADMIN") && password === "12345") {
-    return "Access granted";
-  } else {
-    return "Access denied";
-  }
-}
+// function adminLogin(username, password) {
+//   if ((username === "admin" || username === "ADMIN") && password === "12345") {
+//     return "Access granted";
+//   } else {
+//     return "Access denied";
+//   }
+// }
 
 /*
   Write a method `hows_the_weather` that takes in one argument, a temperature.
@@ -17,19 +17,19 @@ function adminLogin(username, password) {
   between 40 and 65, return "It's a little chilly out there!". If the temperature is above 85,
   return "It's too dang hot out there!". Otherwise, return "It's perfect out there!"
 */
-function howsTheWeather(temperature) {
-  let response;
-  if (temperature < 40) {
-    response = "brisk";
-  } else if (temperature >= 40 && temperature <= 65) {
-    response = "a little chilly";
-  } else if (temperature > 85) {
-    response = "too dang hot";
-  } else {
-    response = "perfect";
-  }
-  return `It's ${response} out there!`;
-}
+// function howsTheWeather(temperature) {
+//   let response;
+//   if (temperature < 40) {
+//     response = "brisk";
+//   } else if (temperature >= 40 && temperature <= 65) {
+//     response = "a little chilly";
+//   } else if (temperature > 85) {
+//     response = "too dang hot";
+//   } else {
+//     response = "perfect";
+//   }
+//   return `It's ${response} out there!`;
+// }
 
 /* 
   Write a method `fizzbuzz` takes in a number. For multiples of three, return
--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,88 @@
 #!/usr/bin/env python3
 
+#    function adminLogin(username, password) {
+#   if ((username === "admin" || username === "ADMIN") && password === "12345") {
+#     return "Access granted";
+#   } else {
+#     return "Access denied";
+#   }
+# }
 def admin_login(username, password):
-    # your code here
+    if username() == "admin" or username() == "ADMIN" and password() == "12345":
+        return "Access granted"
     pass
+
+# // function howsTheWeather(temperature) {
+# //   let response;
+# //   if (temperature < 40) {
+# //     response = "brisk";
+# //   } else if (temperature >= 40 && temperature <= 65) {
+# //     response = "a little chilly";
+# //   } else if (temperature > 85) {
+# //     response = "too dang hot";
+# //   } else {
+# //     response = "perfect";
+# //   }
+# //   return `It's ${response} out there!`;
+# // }
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    response = ""
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
+pass
 
+# function fizzbuzz(num) {
+#   if (num % 3 === 0 && num % 5 === 0) {
+#     return "FizzBuzz";
+#   } else if (num % 3 === 0) {
+#     return "Fizz";
+#   } else if (num % 5 === 0) {
+#     return "Buzz";
+#   } else {
+#     return num;
+#   }
+# }
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
+pass
+
+# function calculator(operation, num1, num2) {
+#   switch (operation) {
+#     case "+":
+#       return num1 + num2;
+#     case "-":
+#       return num1 - num2;
+#     case "*":
+#       return num1 * num2;
+#     case "/":
+#       return num1 / num2;
+#     default:
+#       console.log("Invalid operation!");
+#   }
+# }
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    switcher = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2
+    }
+    return switcher.get(operation, "Invalid operation!")
+
+pass
